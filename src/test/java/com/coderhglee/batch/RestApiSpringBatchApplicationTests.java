72ffce8c58c200bc3166ec259091caa7846f9563
coderhglee <goodna17@gmail.com>
2019-11-09 10:57:39 +0900

    first commit

---
+++ src/test/java/com/coderhglee/batch/RestApiSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderhglee.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
